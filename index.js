1a2eacb8b70993e1e402767e6fe646ae49c8a3bd
Nicolas Alegre <nicolasalegre4@gmail.com>
2022-04-15 20:39:08 -0300

    dom y eventos incluidos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let nombre = prompt ("¿Cual es tu nombre?");
-console.log(nombre);
+alert ("Bienvenido " + nombre.toUpperCase() + " a Thora Bank");
 
 function capturar () { 
    function clientes (nombre, prestamo, tiempo, correo){
@@ -23,28 +23,13 @@ let baseDatos = [];
 function agregar (){
     baseDatos.push (nuevoCliente);
     console.log(baseDatos);
-}
+    document.getElementById("table").innerHTML +='<tbody><td>'+ nuevoCliente.nombre + '</td><td>' + nuevoCliente.prestamo + '</td><td>' + nuevoCliente.tiempo +'</td></tbody>';
+};
 
-
-
-
-
-/* const solicitud1 = new solicitudes ({id:"juan", prestamo: 15000 , duracion:"12 meses"} );
-const solicitud2 = new solicitudes ({id:"ricardo", prestamo: 50000, duracion: "24 meses"});
-const solicitud3 = new solicitudes ({id:"florencia", prestamo: 100000, duracion: "48 meses"});
+const cliente1 = baseDatos.filter ((el) => el.prestamoCapturado > 45000);
+const cliente2 = baseDatos.find ((el) => el.tiempoCapturado === "12 meses"); 
 console.log(cliente1);
 console.log(cliente2);
-console.log(cliente3);
 
-const clientes = [solicitud1, solicitud2, solicitud3];
-
-const resultado = clientes.filter ((el) => el.prestamo > 45000);
-const resultado2 = clientes.find ((el) => el.duracion === "12 meses"); 
-console.log(resultado);
-console.log(resultado2);
-
-clientes.push({id:"rocio", prestamo: 150000, duracion:"72 meses"});
-console.log(clientes);
-
-const morosos = clientes.map ((el) => el.id);
-console.log(morosos); */
+const morosos = baseDatos.map ((el) => el.nombreCapturado);
+console.log(morosos);
