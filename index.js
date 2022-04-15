6cea25534b03cdbdca4cb44ce059c9217df6ed5e
Nicolas Alegre <nicolasalegre4@gmail.com>
2022-04-15 19:21:43 -0300

    correcion de arrays y objetos

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 let nombre = prompt ("¿Cual es tu nombre?");
 console.log(nombre);
 
-let prestamo =parseFloat (prompt ("¿Cuanto dinero estas necesitando?"));
-console.log (prestamo);
-
-let tiempo = prompt ("¿En cuantos meses desea devolverlo?");
-console.log (tiempo);
-
-class solicitudes { 
-    constructor (nombre, prestamo, tiempo){
+function capturar () { 
+   function clientes (nombre, prestamo, tiempo, correo){
         this.nombre = nombre.toUpperCase();
         this.prestamo = parseFloat (prestamo);
         this.tiempo = tiempo;
+        this.correo = correo;
     }
+    let nombreCapturado = document.getElementById("nombre").value;
+    let prestamoCapturado = document.getElementById("prestamo").value;
+    let tiempoCapturado = document.getElementById("duracion").value;
+    let correoCapturado = document.getElementById("correo").value;
+
+    nuevoCliente = new clientes (nombreCapturado, prestamoCapturado, tiempoCapturado, correoCapturado);
+    console.log(nuevoCliente);
+
+    agregar();
 }
 
-const solicitud1 = new solicitudes ({id:"juan", prestamo: 15000 , duracion:"12 meses"} );
+let baseDatos = [];
+function agregar (){
+    baseDatos.push (nuevoCliente);
+    console.log(baseDatos);
+}
+
+
+
+
+
+/* const solicitud1 = new solicitudes ({id:"juan", prestamo: 15000 , duracion:"12 meses"} );
 const solicitud2 = new solicitudes ({id:"ricardo", prestamo: 50000, duracion: "24 meses"});
 const solicitud3 = new solicitudes ({id:"florencia", prestamo: 100000, duracion: "48 meses"});
 console.log(cliente1);
@@ -33,4 +47,4 @@ clientes.push({id:"rocio", prestamo: 150000, duracion:"72 meses"});
 console.log(clientes);
 
 const morosos = clientes.map ((el) => el.id);
-console.log(morosos);
+console.log(morosos); */
