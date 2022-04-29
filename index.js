b2581f77ed5f598652566395d6b0281b5a40bb4b
Nicolas Alegre <nicolasalegre4@gmail.com>
2022-04-29 04:40:41 -0300

    carrusel header, carrusel opiniones,acordeon

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/* -------- ENTRADA POR PROMPT --------*/
 /* let nombre = prompt ("¿Cual es tu nombre?");
 alert ("Bienvenido " + nombre.toUpperCase() + " a Thora Bank"); */
- 
 
+ 
+/* ------ CICLOS Y ARRAY ------- */
 function capturar () { 
    function clientes (nombre, prestamo, tiempo, correo){
         this.nombre = nombre.toUpperCase();
@@ -20,12 +22,33 @@ function capturar () {
     agregar();
 }
 
+/* let resultado = (a,b) => (a*b);
+
+if (nuevoCliente.tiempo < 12){
+    resultado (nuevoCliente.prestamo, (0.08) );
+}
+else if(nuevoCliente.tiempo < 24 ){
+    resultado (nuevoCliente.prestamo, (0.15) );
+}
+else if (nuevoCliente.tiempo <= 36 ){
+    resultado (nuevoCliente.prestamo, (0.20) );
+}
+else {
+    resultado (nuevoCliente.prestamo, (0.35));
+}
+ */
+
+
+/*------- FUNCIONES Y DOM  */
 let baseDatos = [];
 function agregar (){
     baseDatos.push (nuevoCliente);
     console.log(baseDatos);
-    document.getElementById("table").innerHTML +='<tbody><td>'+ nuevoCliente.nombre + '</td><td>' + nuevoCliente.prestamo + '</td><td>' + nuevoCliente.tiempo +'</td></tbody>';
+    document.getElementById("table").innerHTML +='<tbody><td>'+ nuevoCliente.nombre + '</td><td>' + nuevoCliente.prestamo + '</td><td>' + nuevoCliente.tiempo + '</td><td>'  + (nuevoCliente.prestamo * 0.08 + nuevoCliente.prestamo) + '</td></tbody>';
 };
+
+
+
 
 const cliente1 = baseDatos.filter ((el) => el.prestamoCapturado > 45000);
 const cliente2 = baseDatos.find ((el) => el.tiempoCapturado === "12 meses"); 
@@ -34,3 +57,8 @@ console.log(cliente2);
 
 const morosos = baseDatos.map ((el) => el.nombreCapturado);
 console.log(morosos);
+
+/* JSON & STORAGE */
+let usuario = localStorage.getItem(nombre);
+const guardarSesion = (clave, valor) => { sessionStorage.setItem(clave, valor)}; 
+guardarSesion("info cliente", JSON.stringify(nuevoCliente));
