70f3f96b7db942c9b3bb5d57423acbcfd98185f4
Nicolas Alegre <nicolasalegre4@gmail.com>
2022-05-27 20:09:06 -0300

    nueva alerta de entrada

--- js/sweetalert.js
+++ js/sweetalert.js
@@ -1,28 +1,50 @@
 /*------ ALERT ------ */
 (async () => {
-    const {value: nombre } = await Swal.fire({
+  Swal.fire({
         icon: 'question',
-        title:'Ingresa tu Nombre y Apellido',
-        input:'text',
+        title: 'Ingresa tu Nombre y Apellido',
+        input: 'text',
         inputPlaceholder:'Nombre completo...',
-        confirmButtonText: 'Ingresar',
         backdrop:true ,
         width:'60%',
         allowOutsideClick:false,
         allowEscapeKey:false,
         allowEnterKey:false,
-    });
-    
-    if(nombre){
-        Swal.fire({
-            title:'Bienvenido a ThoraBank',
-            text: nombre,
-            confirmButtonText: 'Gracias',
-            backdrop: true , 
-            timer: 3000 ,
-            width: '50%' ,
-            padding: '1rem',
-            icon: 'success',
-        });
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        confirmButtonText: 'Ingresar',
+        showLoaderOnConfirm: true,
+        preConfirm: (login) => {
+          return fetch(`//api.github.com/users/${login}`)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(response.statusText)
+              }
+              return response.json()
+            })
+            .catch(error => {
+              Swal.showValidationMessage(
+                `Vuelve a ingresar tu nombre completo...`
+              )
+            })
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        if (result.isConfirmed){
+          Swal.fire({
+        title:'Bienvenido a ThoraBank',
+        text: 'La mejor prestacion de servicios!!',
+        confirmButtonText: 'Gracias',
+        backdrop: true , 
+        timer: 4000 ,
+        width: '50%' ,
+        padding: '1rem',
+        icon: 'success',
+        allowOutsideClick:false,
+        allowEscapeKey:false,
+        allowEnterKey:false,
+          })
         }
- })() 
+      })
+ })() ;
